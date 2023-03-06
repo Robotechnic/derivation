0938464c825fc5524080924ae7eef9ef647046ca
Robotechnic <robotechnic.developeur@gmail.com>
2023-03-06 12:06:48 +0100

    add a parser to interpret user input as expression

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello World!"
+open Parser
+open Expression
+
+let () = 
+  while true do
+    print_string ">> ";
+    flush stdout;
+    let line = read_line () in
+    let expression = parse line in
+    print_expression expression;
+    print_newline ();
+    print_expression (derivate expression "x");
+    print_newline ();
+  done
---
+++ lib/parser.ml
@@ -0,0 +1,123 @@
+open Expression
+exception SyntaxError of int * string
+
+type pos = int
+type token = 
+  | LPAREN of pos
+  | RPAREN of pos
+  | PLUS of pos
+  | MINUS of pos
+  | TIMES of pos
+  | DIVIDE of pos
+  | POWER of pos
+  | VARIABLE of pos * string
+  | NUMBER of pos * int
+
+let print_token = function
+  | LPAREN (pos) -> Printf.printf "('(', %d)" pos
+  | RPAREN (pos) -> Printf.printf "(')', %d)" pos
+  | PLUS (pos) -> Printf.printf "('+', %d)" pos
+  | MINUS (pos) -> Printf.printf "('-', %d)" pos
+  | TIMES (pos) -> Printf.printf "('*', %d)" pos
+  | DIVIDE (pos) -> Printf.printf "('/', %d)" pos
+  | POWER (pos) -> Printf.printf "('^', %d)" pos
+  | NUMBER (pos, n) -> Printf.printf "('%d', %d)" n pos
+  | VARIABLE (pos, v) -> Printf.printf "('%s', %d)" v pos
+
+let print_tokens tokens = 
+  let rec aux = function
+    | [] -> ()
+    | t::ts -> let () = print_token t in aux ts
+  in aux tokens
+
+let rec parseNumber line j acc =
+  if j >= String.length line then (acc, j)
+  else match String.get line j with
+    | '0'..'9' as c -> parseNumber line (j+1) ((String.make 1 c)::acc)
+    | _ -> (acc, j)
+
+let displayError error i line =
+  let spaces = if i = -1 then String.length line else i in
+  let () = Printf.fprintf stderr "\n%s\n%s^\nError : %s (at char %d)\n" line (String.make spaces ' ') error i in exit 1
+
+let tokenise line =
+  let rec aux i acc =
+    if i >= String.length line then acc
+    else match String.get line i  with
+      | '(' -> aux (i+1) (LPAREN i :: acc)
+      | ')' -> aux (i+1) (RPAREN i :: acc)
+      | '+' -> aux (i+1) (PLUS i :: acc)
+      | '-' -> aux (i+1) (MINUS i :: acc)
+      | '*' -> aux (i+1) (TIMES i :: acc)
+      | '/' -> aux (i+1) (DIVIDE i :: acc)
+      | '^' -> aux (i+1) (POWER i :: acc)
+      | ' ' -> aux (i+1) acc
+      | '0'..'9' ->
+        let (digits, j) = parseNumber line i [] in
+        let number = int_of_string (String.concat "" (List.rev digits)) in
+        aux j (NUMBER (i, number) :: acc)
+      | 'a'..'z' -> aux (i + 1) (VARIABLE (i, String.make 1 (String.get line i)) :: acc)
+      | _ as c -> raise (SyntaxError (i, "Unexpected character : " ^ (String.make 1 c)))
+  in try 
+    List.rev (aux 0 [])
+  with SyntaxError(i, error) ->
+    displayError error i line
+
+let rec factor = function
+| [] -> raise (SyntaxError (-1, "Expected an expression but got nothing"))
+| NUMBER (_, n)::t -> number(n), t
+| VARIABLE (_, v)::t -> var v, t
+| MINUS (p)::t -> (
+  try 
+    let (exp, rest) = factor t in (neg exp, rest)
+  with SyntaxError (ep, error) -> 
+    if ep = -1 then raise (SyntaxError (p, "Expected an expression after '-'"))
+    else raise (SyntaxError (ep, error))
+)
+| PLUS (p)::t -> (
+  try
+    let (exp, rest) = factor t in (exp, rest)
+  with SyntaxError (ep, error) ->
+    if ep = -1 then raise (SyntaxError (p, "Expected an expression after '+'"))
+    else raise (SyntaxError (ep, error))
+)
+| LPAREN(p)::t -> let (exp, rest) = expr None 0 t in
+  (match rest with
+  | RPAREN(_)::t -> exp, t
+  | _ -> raise (SyntaxError (p, "Expected a ')'")))
+| DIVIDE(p)::_ | TIMES(p)::_ | POWER(p)::_ | RPAREN(p)::_ -> raise (SyntaxError (p, "Unexpected token"))
+
+and expr acc level = function
+| [] -> raise (SyntaxError (-1, "Expected an expression but got nothing"))
+| t ->
+  if level >= 3 then factor t
+  else
+    let (leftExpr, rest) = if acc = None then (expr acc (level + 1) t) else (Option.get acc), t 
+  in
+  match rest with
+      | [] -> leftExpr, []
+      | PLUS(_)::t when level = 0 -> let (rightExpr, rightRest) = expr None 0 t in   (add leftExpr rightExpr, rightRest)
+      | MINUS(_)::t when level = 0 -> let (rightExpr, rightRest) = expr None 0 t in  (sub leftExpr rightExpr, rightRest)
+      | TIMES(_)::t when level = 1 -> let (rightExpr, rightRest) = expr None 1 t in  (mul leftExpr rightExpr, rightRest)
+      | DIVIDE(_)::t when level = 1 -> let (rightExpr, rightRest) = expr None 1 t in (div leftExpr rightExpr, rightRest)
+      | POWER(_)::t when level = 2 -> let (rightExpr, rightRest) = expr None 2 t in  (pow leftExpr rightExpr, rightRest)
+      | LPAREN(p)::_ -> raise (SyntaxError (p, "Unexpected parenthesis, expected an operator"))
+      | _ -> leftExpr, rest
+
+let parse line =
+  let tokens = tokenise line in
+  try
+    let (exp, rest) = expr None 0 tokens in
+    match rest with
+    | [] -> exp
+    | h::_ -> 
+      match h with
+      | LPAREN(p) -> raise (SyntaxError (p, "Expected a ')'"))
+      | RPAREN(p) -> raise (SyntaxError (p, "Unexpected ')'"))
+      | PLUS(p) | MINUS(p) | TIMES(p) | DIVIDE(p) | POWER(p) -> raise (SyntaxError (p, "Unexpected operator"))
+      | NUMBER(p, _) -> raise (SyntaxError (p, "Unexpected number"))
+      | VARIABLE(p, _) -> raise (SyntaxError (p, "Unexpected variable"))
+  with SyntaxError(i, error) ->
+    displayError error i line
+ 
+  
