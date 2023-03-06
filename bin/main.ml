e324d3b5a3356b4809b0a1e43ee9a3900314ce59
Robotechnic <robotechnic.developeur@gmail.com>
2023-03-06 12:31:45 +0100

    add argument parser

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,43 @@
 open Parser
 open Expression
 
-let () = 
-  while true do
-    print_string ">> ";
-    flush stdout;
-    let line = read_line () in
-    let expression = parse line in
-    print_expression expression;
-    print_newline ();
-    print_expression (derivate expression "x");
-    print_newline ();
-  done
+let usageMsg = "Usage: " ^ Sys.argv.(0) ^ " [-v VARIABLE] [-e EXPRESSION] [-h]"
+let variable = ref "x"
+let expression = ref ""
+
+let specList = [
+  ("-v", Arg.Set_string variable, "Variable to derivate");
+  ("-e", Arg.Set_string expression, "Expression to derivate")
+]
+
+let stdinDerivate () =
+  try
+    while true do
+      let line = read_line () in
+      let expression = parse line in
+      print_expression (derivate expression !variable);
+      print_newline ();
+    done
+  with End_of_file -> ()
+
+let checkVariable () =
+  if !variable = "" then (
+    Printf.eprintf "No variable specified\n";
+    exit 1
+  ) else if (String.length !variable) > 1 then (
+    Printf.eprintf "Variable must be a single character\n";
+    exit 1
+  ) else if !variable.[0] < 'a' || !variable.[0] > 'z' then (
+    Printf.eprintf "Variable must be a lowercase letter\n";
+    exit 1
+  )
+
+let () =
+  Arg.parse specList (fun arg -> failwith ("Unexpected argument: " ^ arg)) usageMsg;
+  checkVariable();
+  if !expression = "" then
+    stdinDerivate ()
+  else
+    let expression = parse !expression in
+    print_expression (derivate expression !variable);
+    print_newline ()
