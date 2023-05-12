022cf2d7574e3598774615227387545c11e85f76
Robotechnic <robotechnic.developeur@gmail.com>
2023-05-12 17:22:58 +0200

    add ocaml forma

--- bin/main.ml
+++ bin/main.ml
@@ -5,38 +5,43 @@ let usageMsg = "Usage: " ^ Sys.argv.(0) ^ " [-v VARIABLE] [-e EXPRESSION] [-h]"
 let variable = ref "x"
 let expression = ref ""
 
-let specList = [
-  ("-v", Arg.Set_string variable, "Variable to derivate");
-  ("-e", Arg.Set_string expression, "Expression to derivate")
-]
+let specList =
+  [
+    ("-v", Arg.Set_string variable, "Variable to derivate");
+    ("-e", Arg.Set_string expression, "Expression to derivate");
+  ]
 
 let stdinDerivate () =
-  try
-    while true do
+  while true do
+    try
       let line = read_line () in
       let expression = parse line in
       print_expression (derivate expression !variable);
-      print_newline ();
-    done
-  with End_of_file -> ()
+      print_newline ()
+    with End_of_file -> ()
+  done
 
 let checkVariable () =
-  if !variable = "" then (
+  if !variable = ""
+  then (
     Printf.eprintf "No variable specified\n";
-    exit 1
-  ) else if (String.length !variable) > 1 then (
+    exit 1)
+  else if String.length !variable > 1
+  then (
     Printf.eprintf "Variable must be a single character\n";
-    exit 1
-  ) else if !variable.[0] < 'a' || !variable.[0] > 'z' then (
+    exit 1)
+  else if !variable.[0] < 'a' || !variable.[0] > 'z'
+  then (
     Printf.eprintf "Variable must be a lowercase letter\n";
-    exit 1
-  )
+    exit 1)
 
 let () =
-  Arg.parse specList (fun arg -> failwith ("Unexpected argument: " ^ arg)) usageMsg;
-  checkVariable();
-  if !expression = "" then
-    stdinDerivate ()
+  Arg.parse specList
+    (fun arg -> failwith ("Unexpected argument: " ^ arg))
+    usageMsg;
+  checkVariable ();
+  if !expression = ""
+  then stdinDerivate ()
   else
     let expression = parse !expression in
     print_expression (derivate expression !variable);
--- lib/parser.mli
+++ lib/parser.mli
@@ -1,28 +1,23 @@
 type token
 
 exception SyntaxError of int * string
-(**
-	Syntax error at line number [1] with message [2].
-*)
+(** Syntax error at line number [1] with message [2]. *)
 
 val tokenise : string -> token list
-(**
-	Parse the string [1] and return the corresponding expression.
-	@raise SyntaxError if the string is not a valid expression.
-	@param [1] the string to parse.
-	@return the expression corresponding to the string.
-*)
+(** Parse the string [1] and return the corresponding expression.
+
+    @raise SyntaxError if the string is not a valid expression.
+    @param [1] the string to parse.
+    @return the expression corresponding to the string. *)
 
 val parse : string -> Expression.expr
-(**
-	Parse the list of tokens [1] and return the corresponding expression.
-	@raise SyntaxError if the list of tokens is not a valid expression.
-	@param [1] the line to parse.
-	@return the expression corresponding to the list of tokens.
-*)
+(** Parse the list of tokens [1] and return the corresponding expression.
+
+    @raise SyntaxError if the list of tokens is not a valid expression.
+    @param [1] the line to parse.
+    @return the expression corresponding to the list of tokens. *)
 
 val print_tokens : token list -> unit
-(**
-	Print the list of tokens [1] on the standard output.
-	@param [1] the list of tokens to print.
-*)
+(** Print the list of tokens [1] on the standard output.
+
+    @param [1] the list of tokens to print. *)
